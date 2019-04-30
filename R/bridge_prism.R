08bd938c44f58bde2a5abb15ec044b5fb4e3ef8a
Adibi <maadibi@ead.ubc.ca>
2019-04-29 17:05:42 -0700

    samplePatients updated.

--- R/bridge_prism.R
+++ R/bridge_prism.R
@@ -1,14 +1,13 @@
 model_run<-function(model_input=NULL)
 {
-  patient_data<-samplePatients[1,]
-  nms<-names(patient_data)
+   patient_data<-samplePatients[1,]
 
-  if(length(model_input)>0)
-  {
-    patient_data[names(model_input)]<-model_input
-  }
-  else
-    stop("Error: no input parameter was submitted")
+   if(length(model_input)>0)
+   {
+     patient_data[names(model_input)]<-model_input
+   }
+   else
+     stop("Error: no input parameter was submitted")
 
   res<-predictACCEPT(patient_data)
   res<-res[-1:-(length(res)-12)]
