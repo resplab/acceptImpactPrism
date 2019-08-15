b37adf207471ff128ff38fb4345b3b8e44145103
Ainsleigh Hill <ainsleighhill@gmail.com>
2019-08-15 16:47:18 -0700

    Added default input

--- R/bridge_prism.R
+++ R/bridge_prism.R
@@ -22,3 +22,41 @@ model_run<-function(model_input=NULL)
                          calculate_CIs = model_input$calculate_CIs)
     return(as.list(res))
 }
+
+get_default_input <- function() {
+  model_input = list(patient_data = samplePatients)
+  model_input$random_sampling_N = 1000
+  model_input$random_distribution_iteration = 20000
+  model_input$calculate_CIs = TRUE
+  return(flatten_list(model_input))
+}
+
+
+#Gets a hierarchical named list and flattens it; updating names accordingly
+flatten_list<-function(lst,prefix="")
+{
+  if(is.null(lst)) return(lst)
+  out<-list()
+  if(length(lst)==0)
+  {
+    out[prefix]<-NULL
+    return(out)
+  }
+  
+  for(i in 1:length(lst))
+  {
+    nm<-names(lst[i])
+    
+    message(nm)
+    
+    if(prefix!="")  nm<-paste(prefix,nm,sep=".")
+    
+    if(is.list(lst[[i]]))
+      out<-c(out,flatten_list(lst[[i]],nm))
+    else
+    {
+      out[nm]<-lst[i]
+    }
+  }
+  return(out)
+}
