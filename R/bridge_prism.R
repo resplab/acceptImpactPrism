3bdb67ae59e0f466d5892d2288372b31a66cfe97
Ainsleigh Hill <ainsleighhill@gmail.com>
2019-08-15 16:20:11 -0700

    Fixed do.call, edited prism_server function

--- R/bridge_prism.R
+++ R/bridge_prism.R
@@ -1,18 +1,24 @@
 model_run<-function(model_input=NULL)
 {
-   patient_data<-samplePatients[1,]
-
-   if(length(model_input)>0)
-   {
-     patient_data[names(model_input)]<-model_input
-   }
-   else
-     stop("Error: no input parameter was submitted")
-
-  res <- predictACCEPT(patient_data, 
-                     random_sampling_N = model_input$random_sampling_N, 
-                     random_distribution_iteration = model_input$random_distribution_iteration, 
-                     calculate_CIs = model_input$calculate_CIs)
-  
-  return(as.list(res))
+   
+    if (is.null(model_input)) {
+      print("No input parameter specified, using default data")
+      model_input = list(patient_data = samplePatients[1,])
+    } 
+    if(is.null(model_input$patient_data)) {
+      model_input$patient_data = samplePatients[1,]
+    }
+    if(is.null(model_input$random_sampling_N)) {
+      model_input$random_sampling_N = 1000
+    }
+    if(is.null(model_input$random_distribution_iteration)) {
+      model_input$random_distribution_iteration = 20000
+    }
+    if(is.null(model_input$calculate_CIs)) {
+      model_input$calculate_CIs = TRUE
+    }
+    res <- predictACCEPT(model_input$patient_data, random_sampling_N = model_input$random_sampling_N,
+                         random_distribution_iteration = model_input$random_distribution_iteration,
+                         calculate_CIs = model_input$calculate_CIs)
+    return(as.list(res))
 }
