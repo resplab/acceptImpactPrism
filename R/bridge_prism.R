919e3028d9c098748cfb6f83a1f3992581f66a9c
Adibi <maadibi@ead.ubc.ca>
2019-08-26 12:43:37 -0700

    code clean up

--- R/bridge_prism.R
+++ R/bridge_prism.R
@@ -9,15 +9,7 @@
 #' @return Returns a list of results
 model_run<-function(model_input=NULL)
 {
-  model_input <- as.data.frame(model_input)
-  
-    # if (is.null(model_input)) {
-    #   print("No input parameter specified, using default data")
-    #   model_input = list(patient_data = samplePatients[1,])
-    # } 
-    # if(is.null(model_input$patient_data)) {
-    #   model_input$patient_data = samplePatients[1,]
-    # }
+
      if(is.null(model_input$random_sampling_N)) {
        model_input$random_sampling_N = 1000
      }
@@ -27,17 +19,9 @@ model_run<-function(model_input=NULL)
      if(is.null(model_input$calculate_CIs)) {
        model_input$calculate_CIs = TRUE
      }
+    
+    #model_input <- as.data.frame(model_input)
   
-    #patient_data <- tibble::as_tibble(model_input$patient_data)
-   # patient_data <- tibble::as_tibble(model_input$patient_data)
-   #  patient_data <- checkPatientInput(patient_data)
-   #  model_input$patient_data = patient_data
-    
-    #model_input$patient_data <- as_tibble(model_input) %>% select(-starts_with("random"), -starts_with("calculate"))
-    # results = predictACCEPT(model_input$patient_data, random_sampling_N = model_input$random_sampling_N,
-    #                   random_distribution_iteration = model_input$random_distribution_iteration,
-    #                   calculate_CIs = model_input$calculate_CIs)
-    
     results <- predictACCEPT(model_input, random_sampling_N = model_input$random_sampling_N,
                                           random_distribution_iteration = model_input$random_distribution_iteration,
                                           calculate_CIs = model_input$calculate_CIs)
@@ -45,24 +29,6 @@ model_run<-function(model_input=NULL)
     return(as.list(results))
 }
 
-checkPatientInput = function(patientData) {
-  samplePatients = samplePatients[1,]
-  columnNames = names(samplePatients)
-  dataNames = names(patientData)
-  for(name in columnNames) {
-    if(!(name %in% dataNames)) {
-      patientData[[name]] = samplePatients[[name]]
-      message(paste0(name, " is missing in patient input data, setting it to default value"))
-    }
-  }
-  for(name in dataNames) {
-    if(!(name %in% columnNames)) {
-      patientData[[name]] = NULL
-      message(paste0(name, " is not a valid column. Removing from patient input"))
-    }
-  }
-  return(patientData)
-}
 
 get_default_input <- function() {
   model_input = list(patient_data = samplePatients[1,])
