9c7409d95e9d9aeaa66a3ef3d285c8c36c5848b9
Adibi <maadibi@ead.ubc.ca>
2019-08-26 12:38:34 -0700

    fix a bug to return results for non-sense input

--- R/bridge_prism.R
+++ R/bridge_prism.R
@@ -9,29 +9,39 @@
 #' @return Returns a list of results
 model_run<-function(model_input=NULL)
 {
-    if (is.null(model_input)) {
-      print("No input parameter specified, using default data")
-      model_input = list(patient_data = samplePatients[1,])
-    } 
-    if(is.null(model_input$patient_data)) {
-      model_input$patient_data = samplePatients[1,]
-    }
-    if(is.null(model_input$random_sampling_N)) {
-      model_input$random_sampling_N = 1000
-    }
-    if(is.null(model_input$random_distribution_iteration)) {
-      model_input$random_distribution_iteration = 20000
-    }
-    if(is.null(model_input$calculate_CIs)) {
-      model_input$calculate_CIs = TRUE
-    }
-    patient_data = tibble::as_tibble(model_input$patient_data)
-    patient_data = checkPatientInput(patient_data)
-    model_input$patient_data = patient_data
+  model_input <- as.data.frame(model_input)
+  
+    # if (is.null(model_input)) {
+    #   print("No input parameter specified, using default data")
+    #   model_input = list(patient_data = samplePatients[1,])
+    # } 
+    # if(is.null(model_input$patient_data)) {
+    #   model_input$patient_data = samplePatients[1,]
+    # }
+     if(is.null(model_input$random_sampling_N)) {
+       model_input$random_sampling_N = 1000
+     }
+     if(is.null(model_input$random_distribution_iteration)) {
+       model_input$random_distribution_iteration = 20000
+     }
+     if(is.null(model_input$calculate_CIs)) {
+       model_input$calculate_CIs = TRUE
+     }
+  
+    #patient_data <- tibble::as_tibble(model_input$patient_data)
+   # patient_data <- tibble::as_tibble(model_input$patient_data)
+   #  patient_data <- checkPatientInput(patient_data)
+   #  model_input$patient_data = patient_data
     
-    results = predictACCEPT(model_input$patient_data, random_sampling_N = model_input$random_sampling_N,
-                      random_distribution_iteration = model_input$random_distribution_iteration,
-                      calculate_CIs = model_input$calculate_CIs)
+    #model_input$patient_data <- as_tibble(model_input) %>% select(-starts_with("random"), -starts_with("calculate"))
+    # results = predictACCEPT(model_input$patient_data, random_sampling_N = model_input$random_sampling_N,
+    #                   random_distribution_iteration = model_input$random_distribution_iteration,
+    #                   calculate_CIs = model_input$calculate_CIs)
+    
+    results <- predictACCEPT(model_input, random_sampling_N = model_input$random_sampling_N,
+                                          random_distribution_iteration = model_input$random_distribution_iteration,
+                                          calculate_CIs = model_input$calculate_CIs)
+    
     return(as.list(results))
 }
 
@@ -55,12 +65,13 @@ checkPatientInput = function(patientData) {
 }
 
 get_default_input <- function() {
-  model_input = list(patient_data = samplePatients)
+  model_input = list(patient_data = samplePatients[1,])
   model_input$random_sampling_N = 1000
   model_input$random_distribution_iteration = 20000
   model_input$calculate_CIs = TRUE
   return(flatten_list(model_input))
 }
+
 
 
 #Gets a hierarchical named list and flattens it; updating names accordingly
